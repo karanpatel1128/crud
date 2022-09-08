7b028b8710729a8bd361926c11fb8c07109f2dfa
Karan Patel <kpatel74155@gmail.com>
2022-09-08 10:25:33 +0530

    Integrate functionality to create & get Product

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "7415589504",
+    database: "crud_db"
+});
+
+connection.connect(function(error) {
+    if (error) {
+        console.log("Database Error", error);
+    } else {
+        console.log("Database Connected");
+    }
+});
+
 
 app.get("/", function(req, res) {
     let pageData = {
@@ -25,7 +44,36 @@ app.get("/products", function(req, res) {
         title: "All Products",
         pageName: "all-products"
     };
-    res.render("template", pageData);
+    let getAllProducts = "SELECT * FROM products";
+    connection.query(getAllProducts, function(error, result) {
+        if (error) {
+            console.log("Database Query Error ::: ", error);
+        } else {
+            console.log("result", result);
+            pageData.products = result;
+            res.render("template", pageData);
+        }
+    });
+});
+
+app.post("/create-product", function(req, res) {
+    console.log("req.body", req.body);
+    let title = req.body.title;
+    let description = req.body.description;
+    let price = req.body.price;
+    let quantity = req.body.quantity;
+    let isFeatured = req.body.isFeatured;
+    console.log("title", title, description, price, quantity, isFeatured);
+    let insertProduct = `INSERT INTO products(title, description, price, quantity, isFeatured) VALUES('${title}', '${description}', '${price}', '${quantity}', '${isFeatured}')`;
+    console.log("insertProduct", insertProduct);
+    connection.query(insertProduct, function(error, result) {
+        if (error) {
+            console.log("Database Query Error", error);
+        } else {
+            console.log("result", result);
+            res.redirect('/products');
+        }
+    });
 });
 
 /**
@@ -33,5 +81,5 @@ app.get("/products", function(req, res) {
  */
 const port = 4001;
 app.listen(port, function() {
-    console.log(`Server Started at PORT : ${port}`);
+    console.log(`Server Started at PORT :` + port);
 });
